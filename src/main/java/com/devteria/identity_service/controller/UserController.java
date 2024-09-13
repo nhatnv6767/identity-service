e2590facbc678638f89e156bf412a1272534a596
bhnone <>
2024-09-13 23:09:04 +0900

    1 => create get mapping in controller

--- src/main/java/com/devteria/identity_service/controller/UserController.java
+++ src/main/java/com/devteria/identity_service/controller/UserController.java
@@ -4,21 +4,27 @@ import com.devteria.identity_service.dto.request.UserCreationRequest;
 import com.devteria.identity_service.entity.User;
 import com.devteria.identity_service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     // tuong tac truc tiep voi class service
     @Autowired
     private UserService userService;
 
     // endpoint -> di kem voi 1 method
-    @PostMapping("/users")
+    @PostMapping
     // 1 endpoint se nhan data tu user
     // de map data tu request vao voi object thi ta su dung @RequestBody
     User createUser(@RequestBody UserCreationRequest request){
         return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getUsers() {
+
     }
 }
