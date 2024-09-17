f4592f9352a22f33c8e167609c810d8ed265bcb7
bhnone <>
2024-09-18 02:12:20 +0900

    return error with apiResponse

--- src/main/java/com/devteria/identity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devteria/identity_service/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.devteria.identity_service.exception;
 
+import com.devteria.identity_service.dto.request.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,8 +15,11 @@ public class GlobalExceptionHandler {
     // define exception (annotation)
 
     @ExceptionHandler(value = RuntimeException.class)
-    ResponseEntity<String> handlingRuntimeException(RuntimeException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
+    ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception) {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setCode(1001);
+        apiResponse.setMessage(exception.getMessage());
+        return ResponseEntity.badRequest().body(apiResponse);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
