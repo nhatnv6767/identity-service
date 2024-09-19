e3e505e3954efed96955bd9d00122070acbe8ba5
bhnone <>
2024-09-19 19:18:41 +0900

    valid with errorcode : done

--- src/main/java/com/devteria/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/devteria/identity_service/dto/request/UserCreationRequest.java
@@ -11,7 +11,7 @@ public class UserCreationRequest {
     @Size(min = 3, message = "USERNAME_INVALID")
     private String username;
 
-    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Size(min = 8, message = "PASSWORD_INVALID")
     private String password;
     private String firstName;
     private String lastName;
