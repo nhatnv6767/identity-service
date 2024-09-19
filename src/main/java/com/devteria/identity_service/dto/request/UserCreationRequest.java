adf646052ea0051cad84f2141397b44c98c95c5c
bhnone <>
2024-09-19 19:25:23 +0900

    use the same message with Uncategorized but the code different

--- src/main/java/com/devteria/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/devteria/identity_service/dto/request/UserCreationRequest.java
@@ -11,7 +11,7 @@ public class UserCreationRequest {
     @Size(min = 3, message = "USERNAME_INVALID")
     private String username;
 
-    @Size(min = 8, message = "PASSWORD_INVALID_WRONGKEY_TEST")
+    @Size(min = 8, message = "PASSWORD_INVALID")
     private String password;
     private String firstName;
     private String lastName;
--- src/main/java/com/devteria/identity_service/exception/ErrorCode.java
+++ src/main/java/com/devteria/identity_service/exception/ErrorCode.java
@@ -3,7 +3,7 @@ package com.devteria.identity_service.exception;
 public enum ErrorCode {
 
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error"),
-    INVALID_KEY(1001, "Invalid message key"),
+    INVALID_KEY(1001, "Uncategorized error"),
     USER_EXISTED(1002, "User existed"),
     USERNAME_INVALID(1003, "Username must be at least 3 characters"),
     PASSWORD_INVALID(1004, "Password must be at least 8 characters");
