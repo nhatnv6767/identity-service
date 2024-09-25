59802059214286812579f97c0edeae3ede76a96c
bhnone <>
2024-09-25 20:39:17 +0900

    create a dto to receive response -> mapper (mapstruct)

--- src/main/java/com/devteria/identity_service/mapper/UserMapper.java
+++ src/main/java/com/devteria/identity_service/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.devteria.identity_service.mapper;
 
 import com.devteria.identity_service.dto.request.UserCreationRequest;
 import com.devteria.identity_service.dto.request.UserUpdateRequest;
+import com.devteria.identity_service.dto.response.UserResponse;
 import com.devteria.identity_service.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
@@ -10,6 +11,8 @@ import org.mapstruct.MappingTarget;
 public interface UserMapper {
     // method nay se nhan ve 1 parameter la request theo kieu UserCreationRequest va tra ve 1 class kieu la User
     User toUser(UserCreationRequest request);
+
+    UserResponse toUserResponse(User user);
 
     // map data tu UserUpdateRequest vao Object User
     void updateUser(@MappingTarget User user, UserUpdateRequest request);
