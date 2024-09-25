59f2d9f75fd17bc5c221e4576298e0388fa58160
bhnone <>
2024-09-25 20:14:08 +0900

    lombok to set private access modifier to a class the same ..

--- src/main/java/com/devteria/identity_service/dto/request/ApiResponse.java
+++ src/main/java/com/devteria/identity_service/dto/request/ApiResponse.java
@@ -1,40 +1,24 @@
 package com.devteria.identity_service.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
-
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 // xoa dong message = null
 // nghia la field nao null thi khong chen vao Json
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
     // vi du code = 1000 => thanh cong
-    private int code = 1000;
-    private String message;
+    int code = 1000;
+    String message;
     // kieu tra ve co the thay doi
     // phu thuoc vao tung api
-    private T result;
+    T result;
 
-    public int getCode() {
-        return code;
-    }
 
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public T getResult() {
-        return result;
-    }
-
-    public void setResult(T result) {
-        this.result = result;
-    }
 }
