0511fb628e5452c43842944db2ec355b9238d1af
bhnone <>
2024-09-25 21:58:26 +0900

    #6. Encrypt password : authenticate username and password: first create new service

--- src/main/java/com/devteria/identity_service/service/AuthenticationService.java
+++ src/main/java/com/devteria/identity_service/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package com.devteria.identity_service.service;
 
+import com.devteria.identity_service.dto.request.AuthenticationRequest;
+import com.devteria.identity_service.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -9,4 +11,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
+    UserRepository userRepository;
+    boolean authenticate(AuthenticationRequest request){
+        var user = userRepository.fi
+    }
 }
