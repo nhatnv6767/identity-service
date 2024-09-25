218213b320f1acb0f2bd2d315afaa9fb25b986c8
bhnone <>
2024-09-25 20:02:17 +0900

    lombok to gen getter and setter

--- src/main/java/com/devteria/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/devteria/identity_service/dto/request/UserCreationRequest.java
@@ -2,10 +2,12 @@ package com.devteria.identity_service.dto.request;
 
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
 public class UserCreationRequest {
 
     // validation cua Spring thi yeu cau chung ta truyen vao 1 const
