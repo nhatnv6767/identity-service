6f8a41427202d7961bf7d6790515f53b54261bb2
bhnone <>
2024-09-25 21:57:06 +0900

    #6. Encrypt password : authenticate username and password: first create new service

--- src/main/java/com/devteria/identity_service/controller/AuthenticationController.java
+++ src/main/java/com/devteria/identity_service/controller/AuthenticationController.java
@@ -1,8 +1,15 @@
 package com.devteria.identity_service.controller;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationController {
 
 }
