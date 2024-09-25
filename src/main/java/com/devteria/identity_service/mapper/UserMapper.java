2209743e94ec7ddd5749b8fac94f5298a92aee23
bhnone <>
2024-09-25 20:29:44 +0900

    mapstruct Mapper map data inside update user request

--- src/main/java/com/devteria/identity_service/mapper/UserMapper.java
+++ src/main/java/com/devteria/identity_service/mapper/UserMapper.java
@@ -1,11 +1,16 @@
 package com.devteria.identity_service.mapper;
 
 import com.devteria.identity_service.dto.request.UserCreationRequest;
+import com.devteria.identity_service.dto.request.UserUpdateRequest;
 import com.devteria.identity_service.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     // method nay se nhan ve 1 parameter la request theo kieu UserCreationRequest va tra ve 1 class kieu la User
     User toUser(UserCreationRequest request);
+
+    // map data tu UserUpdateRequest vao Object User
+    void updateUser(@MappingTarget User user, UserUpdateRequest request);
 }
