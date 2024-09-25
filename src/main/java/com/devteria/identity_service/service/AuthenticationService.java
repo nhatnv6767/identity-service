2bcfc73de57a3719342c4e47b230833a2c175ffd
bhnone <>
2024-09-25 22:03:33 +0900

    #6. Encrypt password : authenticate username and password: Service done

--- src/main/java/com/devteria/identity_service/service/AuthenticationService.java
+++ src/main/java/com/devteria/identity_service/service/AuthenticationService.java
@@ -7,6 +7,8 @@ import com.devteria.identity_service.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +16,11 @@ import org.springframework.stereotype.Service;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
     UserRepository userRepository;
-    boolean authenticate(AuthenticationRequest request){
-        var user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> AppException(ErrorCode.));
+
+    boolean authenticate(AuthenticationRequest request) {
+        var user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        return passwordEncoder.matches(request.getPassword(), user.getPassword());
     }
 }
