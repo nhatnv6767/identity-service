ccbfc68e1a800915ff3ae8f925933b31baf419bb
bhnone <>
2024-09-25 19:57:39 +0900

    lombok to gen getter and setter

--- src/main/java/com/devteria/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/devteria/identity_service/dto/request/UserCreationRequest.java
@@ -1,9 +1,11 @@
 package com.devteria.identity_service.dto.request;
 
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
+@Getter
 public class UserCreationRequest {
 
     // validation cua Spring thi yeu cau chung ta truyen vao 1 const
@@ -17,43 +19,5 @@ public class UserCreationRequest {
     private String lastName;
     private LocalDate dob;
 
-    public String getUsername() {
-        return username;
-    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
 }
