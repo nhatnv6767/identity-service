1b633d66521a675ab81e10f3185489f3c77ea4b2
bhnone <>
2024-09-25 21:59:49 +0900

    #6. Encrypt password : authenticate username and password: UserRepository

--- src/main/java/com/devteria/identity_service/repository/UserRepository.java
+++ src/main/java/com/devteria/identity_service/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.devteria.identity_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /* 1 request thi di qua 3 layer:
 1. Controller: quan ly Mapping, EndPoint
 2. Service: xu ly cac logic lien quan den Subdomain cua minh
@@ -13,4 +15,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, String> {
     // kiem tra su ton tai cua field username nay voi value username ma ta truyen vao
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
