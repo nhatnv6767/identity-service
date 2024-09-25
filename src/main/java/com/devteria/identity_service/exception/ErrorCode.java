6cde6ae130f81ab8b69598f3c0668659429dc523
bhnone <>
2024-09-25 22:01:55 +0900

    #6. Encrypt password : authenticate username and password: ErrorCode

--- src/main/java/com/devteria/identity_service/exception/ErrorCode.java
+++ src/main/java/com/devteria/identity_service/exception/ErrorCode.java
@@ -6,7 +6,8 @@ public enum ErrorCode {
     INVALID_KEY(1001, "Uncategorized error"),
     USER_EXISTED(1002, "User existed"),
     USERNAME_INVALID(1003, "Username must be at least 3 characters"),
-    PASSWORD_INVALID(1004, "Password must be at least 8 characters");
+    PASSWORD_INVALID(1004, "Password must be at least 8 characters"),
+    USER_NOT_EXISTED(1005, "User not existed");
 
     ErrorCode(int code, String message) {
         this.code = code;
