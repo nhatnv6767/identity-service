31e21f5659d671e387282e4869610caa59cf2497
bhnone <>
2024-09-25 21:40:54 +0900

    #6. Encrypt password : pom

--- src/main/java/com/devteria/identity_service/service/UserService.java
+++ src/main/java/com/devteria/identity_service/service/UserService.java
@@ -11,7 +11,6 @@ import com.devteria.identity_service.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
