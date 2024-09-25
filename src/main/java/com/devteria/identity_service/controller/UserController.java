0052ee862024b9a44450a31fb6e525acc85b1215
bhnone <>
2024-09-25 20:59:35 +0900

    refactoring

--- src/main/java/com/devteria/identity_service/controller/UserController.java
+++ src/main/java/com/devteria/identity_service/controller/UserController.java
@@ -7,17 +7,21 @@ import com.devteria.identity_service.dto.response.UserResponse;
 import com.devteria.identity_service.entity.User;
 import com.devteria.identity_service.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserController {
     // tuong tac truc tiep voi class service
-    @Autowired
-    private UserService userService;
+//    @Autowired
+    UserService userService;
 
     // endpoint -> di kem voi 1 method
 
